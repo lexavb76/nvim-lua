74d72340d5ec3485751441e2ddd0690e96373183
a.babintsev <a.babintsev@inango-systems.com>
2022-03-17 15:45:53 +0500

    added pyright lsp server

---
+++ lua/plugged/lsp/settings/pyright.lua
@@ -0,0 +1,17 @@
+return {
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    --[[ root_dir = function(startpath)
+        return M.search_ancestors(startpath, matcher)
+      end, ]]
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          useLibraryCodeForTypes = true
+        }
+      }
+    },
+    single_file_support = true
+}
