dd66b615100de4cc22e732f340876cf8e1447022
a.babintsev <a.babintsev@inango-systems.com>
2022-03-21 00:22:40 +0500

    Adjusted toggleterminal to open from current directory

--- lua/plugged/toggleterm.lua
+++ lua/plugged/toggleterm.lua
@@ -8,7 +8,7 @@ toggleterm.setup({
     shade_terminals = true,
     shading_factor = 1,
     start_in_insert = true,
-    insert_mappings = true,
+    insert_mappings = false,
     persist_size = true,
     direction = "float", -- 'horizontal', 'vertical', 'tab', 'float'
     close_on_exit = true,
@@ -23,23 +23,60 @@ toggleterm.setup({
     },
 })
 
-local opts = {noremap = true}
+_G.old_current_work_directory = vim.fn.getcwd()
+
+function _G.get_terminal_cd_command()
+    local oldcwd = _G.old_current_work_directory
+    local newcwd = vim.fn.getcwd()
+    local cmd = ''
+    if oldcwd ~= newcwd then
+        _G.old_current_work_directory = newcwd
+        cmd = 'cd '..newcwd
+    end
+    return cmd
+end
+
 -- Keymappings
+function _G.setup_global_mappings()
+    local opts = {silent = true, noremap = true}
+    local conf = require("toggleterm.config").get()
+    local map = vim.api.nvim_buf_set_keymap
+    local unmap = vim.api.nvim_buf_del_keymap
+    local mapping = conf.open_mapping
+    local api = vim.api
+    if mapping then
+        api.nvim_set_keymap("n", mapping,
+            [[<Cmd>lua require'toggleterm'.exec_command('cmd="\n'..get_terminal_cd_command()..'"')<CR>]],
+            opts
+        )
+        if conf.insert_mappings then
+            api.nvim_set_keymap("i", mapping,
+                [[<Esc><Cmd>lua require'toggleterm'.exec_command('cmd="\n'..get_terminal_cd_command()..'"')<CR>]],
+                opts
+            )
+        end
+        pcall(unmap, 0, "t", mapping)
+        map(0, "n", mapping, "<Cmd>ToggleTerm<CR>", opts)
+    end
+end
+
 function _G.set_terminal_keymaps()
     local map = vim.api.nvim_buf_set_keymap
+    local opts = {silent = true, noremap = true}
     map(0, 't', '<Esc>', '<C-\\><C-n>',       opts) -- ESC exit to normal mode in terminal
+    map(0, 'n', '<Esc>', '<Cmd>ToggleTerm<CR>', opts) -- Double ESC exit from terminal
     map(0, "t", "<C-h>", "<C-\\><C-n><C-w>h", opts) -- Windows navigation
     map(0, "t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
     map(0, "t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
     map(0, "t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
-    map(0, "t", "<A-j>", "<C-\\><C-n>:bnext<CR>",     opts)
-    map(0, "t", "<A-k>", "<C-\\><C-n>:bprevious<CR>", opts)
+    -- map(0, "t", "<A-j>", "<C-\\><C-n>:bnext<CR>",     opts)
+    -- map(0, "t", "<A-k>", "<C-\\><C-n>:bprevious<CR>", opts)
     map(0, "t", "<A-C-j>", "<C-\\><C-n>:tabnext<CR>",     opts)
     map(0, "t", "<A-C-k>", "<C-\\><C-n>:tabprevious<CR>", opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- vim.cmd('autocmd! TermOpen term://* lua require("toggleterm.terminal").Terminal:change_dir(vim.fn.getcwd())')
+vim.cmd('autocmd! TermEnter term://* lua setup_global_mappings()') -- Needed to open terminal in current work directory
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
@@ -66,10 +103,8 @@ function _HTOP_TOGGLE()
     htop:toggle()
 end
 
-local bash = Terminal:new({ cmd = "bash", hidden = true, dir = vim.fn.expand('%:p:h') })
+local bash = Terminal:new({ cmd = "bash", hidden = true, dir = vim.fn.expand('%:p:h') }) -- Open in file's directory
 
 function _BASH_TOGGLE()
-    -- bash:exec('cd /tmp')
     bash:toggle()
 end
--- vim.api.nvim_set_keymap('n', '<leader>t', ':lua _BASH_TOGGLE()<cr>', opts)
