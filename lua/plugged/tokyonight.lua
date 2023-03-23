08be96e3fe5238f22c6a1f4325d2c781967c6da3
a.babintsev <a.babintsev@inango-systems.com>
2023-03-23 23:04:37 +0500

    Fixed tokyonight colorscheme setting issue

--- lua/plugged/tokyonight.lua
+++ lua/plugged/tokyonight.lua
@@ -1,28 +1,38 @@
---[[
-tokyonight_italic_comments 	true 	Make comments italic
-tokyonight_italic_keywords 	true 	Make keywords italic
-tokyonight_italic_functions 	false 	Make functions italic
-tokyonight_italic_variables 	false 	Make variables and identifiers italic
-tokyonight_transparent 	false 	Enable this to disable setting the background color
-tokyonight_hide_inactive_statusline 	false 	Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard StatusLine and LuaLine.
-tokyonight_transparent_sidebar 	false 	Sidebar like windows like NvimTree get a transparent background
-tokyonight_dark_sidebar 	true 	Sidebar like windows like NvimTree get a darker background
-tokyonight_dark_float 	true 	Float windows like the lsp diagnostics windows get a darker background.
-tokyonight_colors 	{} 	You can override specific color groups to use other groups or a hex color
-tokyonight_day_brightness 	0.3 	Adjusts the brightness of the colors of the Day style. Number between 0 and 1, from dull to vibrant colors
-tokyonight_lualine_bold 	false 	When true, section headers in the lualine theme will be bold
-vim.g.tokyonight_style = "storm"          -- The theme comes in three styles: ["storm"], a darker variant "night" and "day".
-vim.g.tokyonight_terminal_colors = true   -- [true] Configure the colors used when opening a :terminal
-vim.g.tokyonight_italic_functions = true  -- [false] Make functions italic
-vim.g.tokyonight_sidebars = {             -- Set a darker background on sidebar-like windows.
-    "qf",
-    "vista_kind",
-    "terminal",
-    "packer",
-}
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.g.tokyonight_hide_inactive_statusline = true
---]]
--- Load the colorscheme
-vim.cmd[[colorscheme tokyonight]]
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+})
+
+-- Load the colorscheme when plugin is loaded (wait up to 10 sec)
+require("user.utils").try(10, vim.cmd, [[colorscheme tokyonight]])
