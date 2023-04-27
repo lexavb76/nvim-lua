b4859fa90e39ed71951d3d3c01c449f6f0c63140
a.babintsev <a.babintsev@inango-systems.com>
2023-04-27 16:09:23 +0500

    Fixed pyright lsp server configuring

--- lua/plugged/lsp/settings/pyright.lua
+++ lua/plugged/lsp/settings/pyright.lua
@@ -1,20 +1,31 @@
+local lsp = require("lspconfig")
+local util = lsp.util
 return {
     opts = {
         cmd = { "pyright-langserver", "--stdio" },
         filetypes = { "python" },
-        --[[ root_dir = function(startpath)
-            return M.search_ancestors(startpath, matcher)
-          end, ]]
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              diagnosticMode = "workspace",
-              useLibraryCodeForTypes = true
+        root_dir = function(fname)
+            local root_files = {
+                'pyproject.toml',
+                'setup.py',
+                'setup.cfg',
+                'requirements.txt',
+                'Pipfile',
+                'pyrightconfig.json',
             }
-          }
+            return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or
+                util.path.dirname(fname)
+        end,
+        settings = {
+            python = {
+                analysis = {
+                    autoSearchPaths = true,
+                    diagnosticMode = "workspace",
+                    useLibraryCodeForTypes = false,
+                    typeCheckingMode = "off",
+                },
+            },
         },
-        single_file_support = true,
     },
-    setup = require("lspconfig").pyright.setup,
+    setup = lsp.pyright.setup,
 }
